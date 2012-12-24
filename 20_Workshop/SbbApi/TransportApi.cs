fd21e35c83610b4bcedf3a731fab862ac4225b17
Marko Markovic <marko.markovic@bbv.ch>
2012-12-24 11:45:19 +0100

    MySbbInfo API: extracted interface for the TransportApi and changed return value to IEnumerable<T>.

--- 20_Workshop/SbbApi/TransportApi.cs
+++ 20_Workshop/SbbApi/TransportApi.cs
@@ -1,10 +1,28 @@
-﻿namespace SbbApi
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TransportApi.cs" company="bbv Software Services AG">
+//   Copyright (c) 2012
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// <summary>
+//   Defines the TransportApi type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SbbApi
 {
     using System.Collections.Generic;
     using SbbApi.ApiClasses;
     using ServiceStack.ServiceClient.Web;
 
-    public class TransportApi
+    public class TransportApi : ITransportApi
     {
         private readonly JsonServiceClient serviceClient;
 
@@ -15,18 +33,18 @@
             this.serviceClient = new JsonServiceClient("http://transport.opendata.ch/v1/");
         }
 
-        public List<Connection> GetConnections(string from, string to)
+        public IEnumerable<Connection> GetConnections(string from, string to)
         {
             var connectionResponse = this.serviceClient.Get(new ConnectionRequest { From = @from, To = to, Limit = 2 });
             return connectionResponse.Connections;
         }
 
-        public List<Stationboard> GetStationBoard(string station)
+        public IEnumerable<Stationboard> GetStationBoard(string station)
         {
             return this.serviceClient.Get(new StationBoardRequest { Station = station, Limit = 5}).Stationboard;
         }
 
-        public List<Station> GetLocations(string search)
+        public IEnumerable<Station> GetLocations(string search)
         {
             return this.serviceClient.Get(new LocationRequest { Query = search }).Stations;
         }
