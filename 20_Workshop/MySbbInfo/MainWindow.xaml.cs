5453a2442508d3c4b2423a15e5257c06b897411f
Marko Markovic <marko.markovic@bbv.ch>
2012-12-24 11:46:01 +0100

    MySbbInfo: Extracted TimeTable to custom user control TimeTableControl.

--- 20_Workshop/MySbbInfo/MainWindow.xaml.cs
+++ 20_Workshop/MySbbInfo/MainWindow.xaml.cs
@@ -1,11 +1,27 @@
-﻿namespace MySbbInfo
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="bbv Software Services AG">
+//   Copyright (c) 2012
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MySbbInfo
 {
     using System.Text;
     using System.Windows;
 
     using SbbApi;
-
-    using System.Linq;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -18,27 +34,6 @@
         {
             InitializeComponent();
             this.transportApi = new TransportApi();
-        }
-
-        private void SearchConnectionsClick(object sender, System.Windows.RoutedEventArgs e)
-        {
-            var connections = this.transportApi.GetConnections(this.txtFrom.Text, this.txtTo.Text);
-
-            foreach (var connection in connections)
-            {
-                var from = connection.Sections.First();
-                var to = connection.Sections.Last();
-
-                var formatted = string.Format(
-                    "Abfahrt: {0:HH:mm} Gleis: {1}, Ankunft: {2:HH:mm} auf: Gleis {3}, Dauer: {4:d'.'hh':'mm':'ss}",
-                    from.Departure.Departure,
-                    from.Departure.Platform,
-                    to.Arrival.Arrival,
-                    to.Arrival.Platform,
-                    connection.Duration);
-
-                this.resultList.Items.Add(formatted);
-            }
         }
 
         private void LoadStationboardClick(object sender, RoutedEventArgs e)
