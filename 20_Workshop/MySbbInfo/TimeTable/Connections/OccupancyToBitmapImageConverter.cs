7b7831438774a131bcdc8b3a9b60dc0173ca97fd
Marko Marković <markovic.marko@gmx.ch>
2013-09-18 09:20:46 +0200

    OccupancyToBitmapImageConverter: solved Windows 8 display issue.

--- 20_Workshop/MySbbInfo/TimeTable/Connections/OccupancyToBitmapImageConverter.cs
+++ 20_Workshop/MySbbInfo/TimeTable/Connections/OccupancyToBitmapImageConverter.cs
@@ -73,6 +73,7 @@ namespace MySbbInfo.TimeTable.Connections
                 var bi = new BitmapImage();
                 bi.BeginInit();
                 bi.StreamSource = ms;
+                bi.CacheOption = BitmapCacheOption.OnLoad;
                 bi.EndInit();
 
                 return bi;
